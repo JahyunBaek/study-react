b9e055f2119b611f6fe276e21de6458cb7cbe76d
BJH <baekjahyun@nongshim.co.kr>
2025-12-11 17:58:53 +0900

    [ADD] 렌더링 단계 이해하기 with 리렌더링

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
 
-const Counter = ({onClick}) => {
+const Counter = ({onClick, flag, setFlag}) => {
     const [counter, setCounter] = useState(0);
+    console.log('flag',flag);
+    console.log('setFlag',setFlag);
     const handleCounter = (e) => {
         setCounter(counter + 1);
-        onClick();
+        if(onClick){
+            onClick();
+        }
         //e.stopPropagation();
+    }
+    const handleFlag = (e) => {
+        setFlag(!flag);
+   
     }
     return (
        <>
         <button onClick={handleCounter}>Counter : {counter}</button>
+        <button onClick={(e) => handleFlag(e)}>flag : {String(flag)}</button>
       </>
     )
   }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,16 +2,20 @@ import Counter from './Counter';
 import { useState } from 'react';
 function Main(){
   const [counter, setCounter] = useState(0);
+  const [flag, setFlag] = useState(false);
   const handleMain = () => {
     setCounter(counter + 1);
   }
     return (
       <main>
         <h1>Main Component : {counter}</h1>
+        <h2>flag : {flag.toString()}</h2>
+        <button onClick={() => setFlag(!flag)}>flag 토글</button>
        <Counter onClick={handleMain} />
-       <br />
-       <br />
+       <hr />
        <Counter onClick={handleMain} />
+       <hr />
+       <Counter flag={flag} setFlag={setFlag} />
       </main>
     )
   }
