f3f5cf1ee790407fb73e9b37699f17853f821bc7
BJH <baekjahyun@nongshim.co.kr>
2025-12-11 17:16:57 +0900

    [ADD] state 컴포넌트의 기억저장소

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,8 +1,16 @@
-function Counter() {
+import { useState } from "react";
+
+const Counter = ({onClick}) => {
+    const [counter, setCounter] = useState(0);
+    const handleCounter = (e) => {
+        setCounter(counter + 1);
+        onClick();
+        //e.stopPropagation();
+    }
     return (
-   
-        <button>Counter</button>
-     
+       <>
+        <button onClick={handleCounter}>Counter : {counter}</button>
+      </>
     )
   }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,17 @@
 import Counter from './Counter';
-
+import { useState } from 'react';
 function Main(){
+  const [counter, setCounter] = useState(0);
+  const handleMain = () => {
+    setCounter(counter + 1);
+  }
     return (
       <main>
-       <Counter />
+        <h1>Main Component : {counter}</h1>
+       <Counter onClick={handleMain} />
+       <br />
+       <br />
+       <Counter onClick={handleMain} />
       </main>
     )
   }
