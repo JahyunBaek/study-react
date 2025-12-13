df813e8fda0a05fed04499900fee54d745b76d30
JahyunBaek <backjahyun@naver.com>
2025-12-13 16:30:24 +0900

    [ADDC 객체 업데이틔 하기]

--- src/components/cards/CardForm.jsx
+++ src/components/cards/CardForm.jsx
@@ -1,18 +1,39 @@
+import { useState } from 'react';
 import Card from './Card'
 
 const CardForm = () => {
-
+    const [form,setForm] = useState({
+        title: 'react강의',
+        descriptyion: 'react기초부터 실전까지!'
+    })
     const handleCardForm = (e) =>{
         e.preventDefault();
+    }
+
+    const handleFormChange = (e) =>{
+        console.log(e.target.name)
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value      
+        })
     }
 
     return (
         <>
             <Card>
                 <form style={{ display: 'flex', flexDirection: 'column', gap: '1rem'}} onSubmit={handleCardForm}>
-                    <input type="text" placeholder='강의 제목'></input>
-                    <input type="text" placeholder='강의 한줄 설명'></input>
+                    <input type="text" name='title' value={form.title} placeholder='강의 제목' onChange={handleFormChange}></input>
+                    <input type="text" name='descriptyion' value={form.descriptyion} placeholder='강의 한줄 설명' onChange={handleFormChange}></input>
                     <input type="submit" value='등록'></input>
+                    {
+                        (form.title || form.descriptyion) && (
+                            <div style={{marginTop: '16px', padding: '16px', backgroundColor: '#eee', borderRadius: '6px' }}>
+                              {form.title && (<p>제목 - {form.title}</p>)}
+                              {form.descriptyion && (<p>설명 - {form.descriptyion}</p>)}
+                            </div>
+                        )
+                    }
+                    
                 </form>
             </Card>
         </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppCounter from './AppCounter'
+import App from './AppCard'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //StrictMode 는 개발중에 컴포넌트에서 일반적인 버그를 빠르게 찾을 수 있도록 합니다. 렌더링을 한번  더 함.
   <React.StrictMode>
-    <AppCounter />
+    <App />
   </React.StrictMode>
 )
