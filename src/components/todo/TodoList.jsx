9049b57fdb59027bf49af310d0ec4e3ed090b7d5
JahyunBaek <backjahyun@naver.com>
2025-12-17 22:47:28 +0900

    [ADD] Todo - 완료된 항목보기 기능 추가

--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -1,14 +1,35 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import TodoItem from "./TodoItem"; 
 import { TodoContext } from "../../context/TodoContext";
 const TodoList = () => {
     //const items = [...todos];
     //items.push({id :2, text:'포트폴리오 사이트 만들기'});
     const todos = useContext(TodoContext);
+    const [isDone, setIsDone] = useState(false);
+    const getFilterdTodos = () => {
+        if(!isDone){
+            return todos;
+        }
+        return todos.filter((todo) => todo.done);
+    }
+    const filteredTodos = getFilterdTodos();
+
+    const getStatsCount = () => {
+        const totalCount = todos.length;
+        const doneCount = todos.filter(item => item.done).length;
+        return {totalCount,doneCount};
+    }
+    const {totalCount,doneCount} = getStatsCount()
     return (
+        <>
+        <div>
+            <input id="isDone" type="checkBox"
+            checked={isDone} onChange={(e) => setIsDone(e.target.checked)}></input>
+            <label htmlFor="isDone">완료된 항목 보기({doneCount}/{totalCount})</label>
+        </div>
         <ul>
             {
-                todos.map(item =>(
+                filteredTodos.map(item =>(
                     <li key={item.id}>
                         <TodoItem item={item} />
                     </li>
@@ -17,6 +38,7 @@ const TodoList = () => {
             }
            
         </ul>
+        </>
     )
 }
 
